517e81d34399aa45801852a3c485774e1129c252
jornveltrop <jorn1veltrop@gmail.com>
2021-03-20 18:27:31 +0100

    Aanmaken js profiel en profiel form uitwerken

--- static/scripts/profielToevoegen.js
+++ static/scripts/profielToevoegen.js
@@ -8,3 +8,22 @@ prPic.addEventListener('change', setPreviewProfilePic);
 function setPreviewProfilePic() {
     profilePic.src=URL.createObjectURL(event.target.files[0]);
 }
+
+
+// Button volgende
+
+const buttonVolgende = document.querySelector('#buttonVolgende');
+const profileForm = document.querySelector('.profielToevoegen form');
+
+buttonVolgende.addEventListener('click', volgendeForm);
+
+function volgendeForm() {
+    if (buttonVolgende.innerHTML === "Volgende") {
+        buttonVolgende.innerHTML = "Terug";
+    } 
+    else {
+        buttonVolgende.innerHTML = "Volgende";
+    }
+
+    profileForm.classList.toggle("volgendeForm");
+}
