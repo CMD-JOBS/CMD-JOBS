aaea8f5ed67b18014930920b0918950b3f0e71e5
DylanTroff <dtroff@upcmail.nl>
2021-03-21 16:49:55 +0100

    foutmeldingen

--- passport-config.js
+++ passport-config.js
@@ -7,13 +7,13 @@ function initialize(passport,getUserByEmail, getUserById) {
     const aanmakenUser = async (email, password, done) => { 
         const user = getUserByEmail(email)
         if(user == null){
-            return done(null, false, {message:"Geen email"})
+            return done(null, false, {message:"Er is geen gebruiker met deze email"})
         }
         try {
             if (await bcrypt.compare(password, user.password)){
                 return done(null, user)
             } else{
-                return done(null, false, {message:"wachtwoord fout"})
+                return done(null, false, {message:"Wachtwoord incorrect"})
             }
         }   catch{
                 return done(e)
