8c8c4258673f0689b560d17698f1da8b44284748
jornveltrop <jorn1veltrop@gmail.com>
2021-03-21 01:21:20 +0100

    Verder werken profiel form

--- index.js
+++ index.js
@@ -15,6 +15,36 @@ app.use(express.static('static'));
 app.engine('hbs', hbs({extname: 'hbs'}));
 app.set('view engine', 'hbs');
 
+//BodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Multer setup
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+     callback(null, './uploads');
+  },
+
+  filename: function (req, file, callback){
+     callback(null, Date.now() + '-' + file.originalname)
+  }
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+     fileSize: 1024 * 1024 * 3,
+  },
+});
+
+//Database Jorn
+let db = null;
+
+async function databaseJorn() {
+  const uri = "mongodb+srv://Ashley:mongodbWW@cluster0.ivfex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const options = { useNewUrlParser: true, useUnifiedTopology: true };
+  const client = new MongoClient(uri, options);
+  db = await client.db('users');
+};
+
 // Routes
 app.get('/', (req, res) => {
   res.render('homepagina')
@@ -30,6 +60,36 @@ app.get('/account', (req, res) => {
 
 app.get('/profielToevoegen', (req, res) => {
   res.render('profielToevoegen')
+});
+
+app.post('/profielToevoegen', upload.single('pFoto'), async (req,res) => {
+  await databaseJorn()
+   .then(() => {
+      //Succesvolle verbinding
+      console.log('We have a connection to Mongo!')
+   })
+   .catch( error => {
+      //Error bij verbinden
+      console.log(error)
+   });
+
+  const id = slug(req.body.vNaam + req.body.aNaam);
+  const pFotoPath = '../uploads/' + req.file.filename;
+  const profiel = {
+                    'id': id, 
+                    'profileImg': pFotoPath, 
+                    'firstName': req.body.vNaam, 
+                    'lastName': req.body.aNaam, 
+                    'opleidingsNiveau': req.body.opleidingsNiveau, 
+                    'biografie': req.body.biografie, 
+                    'functie': req.body.functie, 
+                    'dienstverband': req.body.dienstverband,
+                    'bedrijfsgrootte': req.body.bedrijfsgrootte
+                  };
+  await db.collection('profielen').insertOne(profiel);
+  res.render('profielPagina', {title: 'Nieuw Profiel', profiel})
+
+  client.close();
 });
 
 // 404 pagina
--- static/scripts/profielToevoegen.js
+++ static/scripts/profielToevoegen.js
@@ -1,12 +1,12 @@
 console.log ("hello");
 
-// Preview uploaded profilePic
-const prPic = document.querySelector('#prPic');
+// Preview uploaded profielFoto
+const pFoto = document.querySelector('#pFoto');
 
-prPic.addEventListener('change', setPreviewProfilePic);
+pFoto.addEventListener('change', setPreviewProfielFoto);
 
-function setPreviewProfilePic() {
-    profilePic.src=URL.createObjectURL(event.target.files[0]);
+function setPreviewProfielFoto() {
+    profielFoto.src=URL.createObjectURL(event.target.files[0]);
 }
 
 
@@ -18,6 +18,8 @@ const formActive1 = document.querySelector('.profielToevoegen > div:last-of-type
 const formActive2 = document.querySelector('.profielToevoegen > div:last-of-type span:last-of-type');
 
 buttonVolgende.addEventListener('click', volgendeForm);
+formActive1.addEventListener('click', volgendeForm);
+formActive2.addEventListener('click', volgendeForm);
 
 function volgendeForm() {
     if (buttonVolgende.innerHTML === "Volgende") {
@@ -31,3 +33,48 @@ function volgendeForm() {
     formActive1.classList.toggle('formActive');
     formActive2.classList.toggle('formActive');
 }
+
+//Form errors
+const pFotoField = document.querySelector('.pFoto > span');
+const vNaamField = document.querySelector('input[name="vNaam"]');
+const aNaamField = document.querySelector('input[name="aNaam"]');
+const opleidingsNiveauField = document.querySelector('select[name="opleidingsNiveau"]');
+const biografieField = document.querySelector('textarea[name="biografie"]');
+const functieField = document.querySelector('select[name="functie"]');
+const dienstverbandField = document.querySelector('select[name="dienstverband"]');
+const bedrijfsgrootteField = document.querySelector('select[name="bedrijfsgrootte"]');
+
+//Invalid fields rode achtergrond geven
+Array.prototype.slice.call(document.querySelectorAll("[required]")).forEach(function(input){
+    input.addEventListener('invalid',function(e){
+        
+        if (this.name == 'pFoto' || this.name == 'vNaam' || this.name == 'aNaam' || this.name == 'opleidingsNiveau' || this.name == 'biografie') {
+            volgendeForm();
+        }
+    
+        if (this.name == 'pFoto') {
+            pFotoField.classList.add('error');
+        }
+        else if (this.name == 'vNaam') {
+            vNaamField.classList.add('error');
+        }
+        else if (this.name == 'aNaam') {
+            aNaamField.classList.add('error');
+        }
+        else if (this.name == 'opleidingsNiveau') {
+            opleidingsNiveauField.classList.add('error');
+        }
+        else if (this.name == 'biografie') {
+            biografieField.classList.add('error');
+        }
+        else if (this.name == 'functie') {
+            functieField.classList.add('error');
+        }
+        else if (this.name == 'dienstverband') {
+            dienstverbandField.classList.add('error');
+        }
+        else if (this.name == 'bedrijfsgrootte') {
+            bedrijfsgrootteField.classList.add('error');
+        }
+    })
+});
