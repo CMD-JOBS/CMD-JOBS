a981ad07a791a0b25cf5ad440bf01f5da64607dc
YunusEmreCMD <y.emre-alkan@hotmail.com>
2021-03-21 20:12:31 +0100

    profielpagina af, DB connectie gelukt, nieuwe views - Alleen bezig met data versturen

--- index.js
+++ index.js
@@ -3,16 +3,73 @@ const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const slug = require('slug');
 const multer = require('multer');
-const { MongoClient } = require('mongodb')
 const app = express();
+const dotenv = require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { ObjectID } = require('mongodb')
 // const sass = require('node-sass');
+
+console.log(process.env.TESTVAR);
+
 const port = 3000;
+
+
+// let db = null;
+// let resultatenCollection = null;
+// let opgeslagenCollection = null;
+
+//   const uri = process.env.DB_URI
+//   const options = {
+//     useUnifiedTopology: true
+//   };
+//   const client = new MongoClient(uri, options)
+
+
+//   // await client.connect();
+//   // db = await client.db(process.env.DB_NAME)
+
+
+// async function databaseEmre() {
+//   await client.connect();
+//   db = await client.db(process.env.DB_NAME);
+//   resultatenCollection = await db.collection('resultaten');
+//   opgeslagenCollection = await db.collection('opgeslagen');
+// }
+
+let db = null;
+
+// functie om de database te connecten
+async function connectDB() {
+  const uri = process.env.DB_URI
+  const options = {
+    useUnifiedTopology: true
+  };
+  const client = new MongoClient(uri, options)
+  await client.connect();
+  db = await client.db(process.env.DB_NAME)
+}
+// connectDB()
+//   .then(() => {
+//     // Het verbinden met de DB is gelukt
+//     console.log('Feest!')
+//   })
+//   .catch(error => {
+//     // Het verbinden met de DB is niet gelukt
+//     console.log(error)
+//   });
 
 // Aangeven waar onze statische files zich bevinden  
 app.use(express.static('static'));
 
+//BodyParser
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 // Template engine opgeven
-app.engine('hbs', hbs({extname: 'hbs'}));
+app.engine('hbs', hbs({
+  extname: 'hbs'
+}));
 app.set('view engine', 'hbs');
 
 // Routes
@@ -24,9 +81,73 @@ app.get('/resultaten', (req, res) => {
   res.render('resultaten')
 });
 
+// app.get('/profiel', async (req, res) => {
+//   let profielen = {}
+//   profielen = await db.collection('profielen').find({}, {
+//   }).toArray();
+//   res.render('profiel', {
+//     title: "test",
+//     profielen,
+//   });
+// });
+
+
 app.get('/profiel', (req, res) => {
-  res.render('profiel', { title: "JobDone" })
+  res.render('profiel')
 });
+
+app.post('/profiel', async (req, res) => {
+
+  console.log(req.body.functie);
+
+  await connectDB()
+
+  .then(() => {
+    // if succesful connection is made show a message
+    console.log('we have a connection to mongo!');
+  })
+  .catch((error) => {
+    // if connection is unsuccesful, show errors
+    console.log(error);
+  });
+  
+  const profielen = {
+    "biografie": req.body.biografie,
+    "opleidingsNiveau": req.body.opleidingsNiveau,
+    "functie": req.body.functie,
+    "dienstverband": req.body.dienstverband,
+    "bedrijfsgrootte": req.body.bedrijfsgrootte
+  };
+  await db.collection('profielen').insertOne(profielen);
+
+  res.render('profiel', {
+    title: "test",
+    profielen
+  })
+});
+
+
+// app.post('/profiel', async (req, res) => {
+//   const id = slug(req.body.naam);
+//   const profiel = {
+//     "voornaam": req.body.vNaam,
+//     "achternaam": req.body.aNaam,
+//     "biografie": req.body.biografie,
+//     "opleidingsNiveau": req.body.opleidingsNiveau,
+//     "functie": req.body.functie,
+//     "dienstverband": req.body.dienstverband,
+//     "bedrijfsgrootte": req.body.bedrijfsgrootte
+//   };
+//   await db.collection('profielen').insertOne(profiel);
+//   res.render('profiel', {
+//     title: "hoi",
+//     profiel
+//   })
+// });
+
+
+// TEST
+// TEST
 
 // 404 pagina
 app.use(function (req, res, next) {
--- static/scripts/javascript.js
+++ static/scripts/javascript.js
@@ -1,1 +1,45 @@
-console.log ("hello");
+console.log("hello");
+
+var bewerkKnop = document.getElementById("bewerk-knop");
+
+function textareaBewerken() {
+    var textarea = document.querySelector("textarea");
+    textarea.classList.toggle("bewerk-textarea");
+}
+bewerkKnop.addEventListener("click", textareaBewerken);
+
+
+function selectBewerken() {
+    var selectBox, i;
+    selectBox = document.querySelectorAll("select");
+    for (i = 0; i < selectBox.length; i++) {
+        selectBox[i].classList.toggle("bewerk-select");
+    }
+}
+bewerkKnop.addEventListener("click", selectBewerken);
+
+
+function showBewerktVerzendKnop() {
+    var bewerktVerzendKnop = document.getElementById("bewerkt-verzend-knop");
+    bewerktVerzendKnop.classList.toggle("laat-bewerkt-verzend-knop-zien");
+}
+bewerkKnop.addEventListener("click", showBewerktVerzendKnop);
+
+
+// var bewerkKnop = document.getElementById("bewerk-knop");
+
+// function textareaBewerken() {
+//     var textarea = document.querySelector("textarea");
+//     textarea.classList.toggle("bewerk-textarea");
+
+//     var selectBox, i;
+//     selectBox = document.querySelectorAll("select");
+//     for (i = 0; i < selectBox.length; i++) {
+//         selectBox[i].classList.toggle("bewerk-select");
+//     }
+
+//     var bewerktVerzendKnop = document.getElementById("bewerkt-verzend-knop");
+//     bewerktVerzendKnop.classList.toggle("laat-bewerkt-verzend-knop-zien");
+
+// }
+// bewerkKnop.addEventListener("click", textareaBewerken); 
