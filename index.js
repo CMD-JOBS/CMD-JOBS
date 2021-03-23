abeb5243077c12726f9a42cc9d123d1290ce8152
DylanTroff <dtroff@upcmail.nl>
2021-03-23 16:48:07 +0100

    werkende login functie

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ if (process.env.NODE_ENV !== 'production'){
 // requires
 const express = require('express');
 const hbs = require('express-handlebars');
-const bodyParser = require('body-parser');
 const slug = require('slug');
 const multer = require('multer');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const { ObjectID } = require('mongodb');
 const app = express();
 const passport = require('passport')
-const localStrategy = require('passport-local')
-const flash = require('express-flash');
+const localStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
 const session = require('express-session'); 
+const bcrypt = require('bcryptjs');
 const port = 3000;
 // const sass = require('node-sass');
 
@@ -26,7 +26,73 @@ app.engine('hbs', hbs({extname: 'hbs'}));
 app.set('view engine', 'hbs');
 
 //BodyParser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+//Express Session
+app.use(session({
+  secret: 'back-end is echt leuk',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Passport middelware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Flash
+app.use(flash());
+
+//Global
+app.use((req, res, next) => {
+  res.locals.succes_msg = req.flash('succes_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+//passport
+passport.use(
+  new localStrategy({ usernameField: 'email' }, (email, password, done)=>{
+    // Match user
+    User.findOne({ email: email})
+    .then(user => {
+      if(!user){
+        return done(null, false, {message: 'Email bestaat nog niet'})
+      }
+
+      // Match Password
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+        if(isMatch){
+          return done(null, user);
+        } else{
+          return done(null, false, {message:'Wachtwoord fout'})
+        }
+      });
+    })
+    .catch(err => console.log(err));
+  })
+);
+
+passport.serializeUser((user, done) =>{
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id,(err, user) => {
+    done(err, user);
+  });
+});
+
+//Authenticate check
+function ensureAuthenticated (req, res, next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  req.flash('error_msg','Werkt niet');
+  res.redirect('/')
+
+}
 
 //Multer setup
 const storage = multer.diskStorage({
@@ -45,109 +111,113 @@ const upload = multer({
   },
 });
 
-// Gebruikt voor Hashen passwords
-const bcrypt = require('bcrypt');
-
-// Login benodigdheden 
-const initializepassport = require('./passport-config');
-initializepassport (
-  passport,
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-);
-
-app.use(flash());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-const users = [];
-
-//Zorgen dat iemand is ingelogt voordat hij bij de resultaten kan
-function checkAuthenticated (req, res, next) {
-  if(req.isAuthenticated()){
-    return next()
-  }
-  res.redirect('/')
-};
-
-function checkNotAuthenticated (req, res, next) {
-  if(req.isAuthenticated()){
-    res.redirect('/resultaten');
-  };
-  next();
-}; 
-
 
 //Database connectie functie
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+
+});
+
+const User = mongoose.model("User", UserSchema);
+
+const uri = process.env.DB_URI;
+
+mongoose.connect(uri, {useNewUrlParser: true})
+.then(() => console.log('MongoDB connectie :)'))
+.catch(err => console.log(err));
+
 let db = null;
 let resultatenCollection = null;
 let opgeslagenCollection = null;
 
-async function connectDB() {
-  const uri = process.env.DB_URI
-  const options = {
-    useUnifiedTopology: true
-  };
-  const client = new MongoClient(uri, options);
-  await client.connect();
-  db = await client.db(process.env.DB_NAME);
-  resultatenCollection = await db.collection('resultaten');
-  opgeslagenCollection = await db.collection('opgeslagen');
-}
+
+// resultatenCollection = await db.collection('resultaten');
+// opgeslagenCollection = await db.collection('opgeslagen');
+
 
 // Routes
-app.get('/',checkNotAuthenticated, (req, res) => {
+
+app.get('/', (req, res) => {
   res.render('inloggen')
 });
 
-app.post('/', passport.authenticate('local', {
-  successRedirect: '/resultaten',
-  failureRedirect: '/',
-  failureFlash: true
-}));
-
-app.get('/registreren',checkNotAuthenticated, (req, res) => {
+app.get('/registreren', (req, res) => {
   res.render('registreren')
 });
 
-app.post('/registreren', async (req, res) => {
-  await connectDB()
-  .then(() => {
-    // if succesful connection is made show a message
-    console.log('we have a connection to mongo!');
-  })
-  .catch((error) => {
-    // if connection is unsuccesful, show errors
-    console.log(error);
-  });
-  try {
-    // Database inloggen gebruiker
-    const hashedWachtwoord = await bcrypt.hash(req.body.password, 10)
-    const gebruiker = {
-      email: req.body.email,
-      password: hashedWachtwoord
-    }
-    await db.collection('profielen').insertOne(gebruiker);
+app.post('/registreren', (req, res) => {
+  const { email, password} = req.body;
+  let errors = [];
 
-    //inlog gebruiker
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedWachtwoord
-    })
-    res.render('profielToevoegen', {gebruiker});
-  } catch{
-    res.redirect('/registreren')
+  //Controleer benodigden velden
+  if (!email || !password){
+    errors.push({message:'please invullen'});
   }
-  console.log(users)
-})
 
-app.get('/profielToevoegen', checkAuthenticated, (req, res) => {
+  //Wachtwoord lengte instellen
+  if(password.length <2) {
+    errors.push({message: 'wachtwoord te kort'})
+  }
+
+  if(errors.length >0){
+    res.render('registreren',{
+      errors,
+      email,
+      password
+    })
+  } else {
+    // Inlog goedkeuring
+    User.findOne({ email: email })
+    .then(user =>{
+      if(user) {
+        //Gebruiker bestaat
+        errors.push({message:"Email al in gebruik"});
+        res.render('registreren',{
+          errors,
+          email,
+          password
+        });
+      } else{
+        const newUser = new User({
+          email,
+          password
+        });
+        //hashedPassword
+        bcrypt.genSalt(10, (err, salt) => 
+          bcrypt.hash(newUser.password, salt,(err, hash)=>{
+            if(err) throw err;
+            // Verander naar Hash password
+            newUser.password = hash;
+            //save gebruiker
+            newUser.save()
+              .then(user =>{
+                req.flash('succes_msg','geregistreerd');
+                res.render('profielToevoegen');
+              })
+              .catch(err => console.log(err));
+        }));
+      }
+    });
+  }
+});
+
+// Login handle
+app.post('/', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect:'/resultaten',
+    failureRedirect: '/',
+    failureFlash: true
+  })(req, res, next);
+});
+
+app.get('/profielToevoegen', ensureAuthenticated,(req, res) => {
   res.render('profielToevoegen')
 });
 
@@ -182,7 +252,7 @@ app.post('/profielToevoegen', upload.single('pFoto'), async (req,res) => {
 
 
 // Profiel pagina
-app.get('/profiel', checkAuthenticated, async (req, res) => {
+app.get('/profiel', async (req, res) => {
   await connectDB()
     .then(() => {
       // if succesful connection is made show a message
@@ -206,7 +276,7 @@ app.get('/profiel', checkAuthenticated, async (req, res) => {
   }
 });
 
-app.post('/profiel', async (req, res) => {
+app.post('/profiel', ensureAuthenticated,async (req, res) => {
 
   await connectDB()
     .then(() => {
@@ -235,45 +305,46 @@ app.post('/profiel', async (req, res) => {
 });
 
 // ashley werkt in deze route 
-app.get('/resultaten', checkAuthenticated, async (req, res) => {
+app.get('/resultaten',ensureAuthenticated, async (req, res) => {
+  res.render('resultaten')
   // de functie voor de opslaan  optie
-  const objectID = new ObjectID('6058ba04e8d259e2d0e7def7');
- opgeslagenCollection.find({ _id: objectID }, (err, opslaanObject) => {
+//   const objectID = new ObjectID('6058ba04e8d259e2d0e7def7');
+//  opgeslagenCollection.find({ _id: objectID }, (err, opslaanObject) => {
 
-  if (err) {
-    console.log(err); // als er een error dan showt het een error
-  } else if (opslaanObject.opgeslagen) {
+//   if (err) {
+//     console.log(err); // als er een error dan showt het een error
+//   } else if (opslaanObject.opgeslagen) {
 
-    resultatenCollection // in de profile collection wordt die profiel verwijdert
-      .find({ _id: { $in: opslaanObject.opgeslagen } }) // nin = not in array //find id uit de database
-      .toArray((err, users) => {
-        // add de gelikede profielen worden in een array geplaatst en in de collectie van likes.
-        if (err) {
-          console.log(err);
-        } else {
-          res.render('resultaten', {
-            title: 'Een lijst met resultaten',
-            users,
-          });
-        }
-      });
-  } else {
-    // anders wordt het aan een array toegevoed
-  resultatenCollection.find({}).toArray((err, users) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('resultaten', {
-          title: 'Een lijst met resultaten',
-          users,
-        });
-      }
-    });
-  }
+//     resultatenCollection // in de profile collection wordt die profiel verwijdert
+//       .find({ _id: { $in: opslaanObject.opgeslagen } }) // nin = not in array //find id uit de database
+//       .toArray((err, users) => {
+//         // add de gelikede profielen worden in een array geplaatst en in de collectie van likes.
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           res.render('resultaten', {
+//             title: 'Een lijst met resultaten',
+//             users,
+//           });
+//         }
+//       });
+//   } else {
+//     // anders wordt het aan een array toegevoed
+//   resultatenCollection.find({}).toArray((err, users) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render('resultaten', {
+//           title: 'Een lijst met resultaten',
+//           users,
+//         });
+//       }
+//     });
+//   }
+// });
 });
-});
 
-app.get('/opgeslagenvacatures', checkAuthenticated, async (req, res) => {
+app.get('/opgeslagenvacatures',ensureAuthenticated, async (req, res) => {
   const objectID = new ObjectID('6058ba04e8d259e2d0e7def7');
   // object van de eerste id
   opgeslagenCollection.findOne({ _id: objectID }, (err, opslaanObject) => {
