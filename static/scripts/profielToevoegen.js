09f7712ed91a1a7050442b04550a6d6ae457129c
jornveltrop <jorn1veltrop@gmail.com>
2021-03-28 09:28:55 +0200

    SetPreviewFunction

--- static/scripts/profielToevoegen.js
+++ static/scripts/profielToevoegen.js
@@ -38,39 +38,6 @@ formActive2.addEventListener('click', volgendeForm);
 
 
 // #3 Preview & validate uploaded profielFoto
-
-function fileValidation() {
-
-      
-    var filePath = fileInput.value;
-  
-    // Allowing file type
-    var allowedExtensions = 
-            /(\.jpg|\.jpeg|\.png|\.gif)$/i;
-      
-    if (!allowedExtensions.exec(filePath)) {
-        alert('Invalid file type');
-        fileInput.value = '';
-        return false;
-    } 
-    else 
-    {
-      
-        // Image preview
-        if (fileInput.files && fileInput.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                document.getElementById(
-                    'imagePreview').innerHTML = 
-                    '<img src="' + e.target.result
-                    + '"/>';
-            };
-              
-            reader.readAsDataURL(fileInput.files[0]);
-        }
-    }
-}
-
 function setPreviewProfielFoto() {
     const filePath = pFoto.value;
     const allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
